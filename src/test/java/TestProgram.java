e3c999fed541bb59c32e883839659557584a2e5f
zoders <sanyafil61802@gmail.com>
2022-08-10 03:36:22 +0300

    created second test

--- src/test/java/TestProgram.java
+++ src/test/java/TestProgram.java
@@ -4,8 +4,18 @@ import org.junit.Test;
 
 public class TestProgram {
     @Test
-    public void iCanDownloadThirdPic() throws InterruptedException, MalformedURLException {
+    public void iCanDownloadThirdPicWithChrome() throws InterruptedException, MalformedURLException {
         GooglePage gp = new GooglePage("chrome", true);
+        gp.searchFor("Александр Филипповский");
+        Thread.sleep(1000);
+        gp.switchToTab("Картинки");
+        // нумерация на атрибуте data-ri c 0
+        gp.selectAndDownloadPicByNumber(2);
+    }
+
+    @Test
+    public void iCanDownloadThirdPicWithFirefox() throws InterruptedException, MalformedURLException {
+        GooglePage gp = new GooglePage("firefox", true);
         gp.searchFor("Александр Филипповский");
         Thread.sleep(1000);
         gp.switchToTab("Картинки");
